56fd6d77e36a3be85909b1c891444e4dc86d74c1
gavuolo <gabs.vuolo@gmail.com>
2022-07-04 16:49:15 -0400

    tela de quizzes armazenados

--- projeto6-buzzquizz/script1.js
+++
@@ -1,7 +0,0 @@
-promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-promessa.then(executarPromessa);
-
-function executarPromessa(resposta){
-    console.log(resposta);
-    console.log(resposta.data);
-}
